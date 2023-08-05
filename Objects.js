087a2efd7c16944a23ee2147af345cd4300c0a7f
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-08-05 23:34:39 +0600

    add object

---
+++ Objects.js
@@ -0,0 +1,16 @@
+let ar={
+  fastName : 'Arman',
+  lastName : 'Akash',
+  location : {
+    city :'Sirajganj',
+    zip : 6700,
+  }
+};
+
+
+// *
+// console.log(ar);
+// **
+// console.log(ar['lastName']);
+// ***
+// console.log(ar.location.city);
