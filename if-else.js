56b9f6371f43f0b9be80433b96a8fc5a44f1bb6f
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-08-07 22:27:11 +0600

    if and else add kora holo

---
+++ if-else.js
@@ -0,0 +1,15 @@
+var age= 18; //Yes NID Apply
+// var age= 17; //NID Not Apply
+// var age= "a"; //Worng Info
+
+if(age>=18){
+console.log("Yes NID Apply");
+}
+
+else if(age<18){
+  console.log("NID Not Apply");
+}
+
+else{
+  console.log("Worng Info");
+}
