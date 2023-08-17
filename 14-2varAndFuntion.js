b20f670270403a9cc940644a4ae465fcbc2f0346
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-08-18 00:05:31 +0600

    14-2fileTa Add holo

---
+++ 14-2varAndFuntion.js
@@ -0,0 +1,14 @@
+function inchToFeet(inch){
+  var feet = inch / 12;
+  return feet;
+}
+
+var akashFeet=inchToFeet(71);
+console.log(akashFeet);
+
+var sagorFeet= inchToFeet(73);
+console.log(sagorFeet);
+
+var kayesFeet= inchToFeet(48);
+console.log(kayesFeet);
+
--- funtion.js
+++ funtion.js
@@ -1,13 +1,13 @@
-// function adder (a, b){
-//   console.log(a + b);
-// }
-// adder(10, 5);
+function adder (a, b){
+  console.log(a + b);
+}
+adder(10, 5);
 
-        // funtionExpression
-// let ar =function adder (a,b){
-//   return a+b;
-// };
-// console.log(ar(10,20)+10);
+        funtionExpression
+let ar =function adder (a,b){
+  return a+b;
+};
+console.log(ar(10,20)+10);
 
 
 
