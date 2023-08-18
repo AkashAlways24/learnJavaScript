0e83169806bc69488b793b35d3420a8da5b36a92
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-08-19 00:27:58 +0600

    function add 14-05

--- 14-05factorial.js
+++ 14-05factorial.js
@@ -1,6 +1,24 @@
-var factorial = 1;
+// var factorial = 1;
 
-for (let i = 1; i <= 5 ; i++) {
-  factorial = factorial * i;
-  console.log(i,factorial);
+// for (let i = 1; i <= 5 ; i++) {
+//   factorial = factorial * i;
+//   console.log(i,factorial);
+// }
+
+
+function factorial(num) {
+  var factorial = 1;
+  for(let i = 1; i <= num; i++){
+    factorial = factorial * i;
+  }
+  return factorial
 }
+
+var result = factorial();
+console.log(result);
+
+
+
+
+
+
