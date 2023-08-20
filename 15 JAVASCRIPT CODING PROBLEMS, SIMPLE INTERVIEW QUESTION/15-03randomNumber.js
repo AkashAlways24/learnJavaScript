ae287c12e66eae94a2c0fd501f739eeb1a5d2fdf
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-08-21 00:05:52 +0600

    15-01 k 15-02 nam change and 15-03 random num add

---
+++ 15 JAVASCRIPT CODING PROBLEMS, SIMPLE INTERVIEW QUESTION/15-03randomNumber.js
@@ -0,0 +1,15 @@
+var num = 2.499;
+var result= Math.floor(num);  //dosomij jai thakuk necher man pabe jemon =2
+var result2= Math.ceil(num); //dosomij jai thakuk opore man pabe jemon = 3
+var result3= Math.round(num); //dosomik er por .5 hole opore r .49 hole necher man= 3, 2
+
+//console.log(result, result2, result3);
+
+
+// lodu er moto random Number
+
+var randomNumber = Math.random()*6;  //6 thke nana rokom random number asbe
+
+var output = Math.round(randomNumber);  //round er douyar karone dosomic asbe na round er man asbe
+
+console.log(output);
