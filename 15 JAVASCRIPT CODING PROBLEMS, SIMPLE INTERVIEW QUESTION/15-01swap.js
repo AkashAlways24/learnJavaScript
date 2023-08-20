5b2124bfbf2bdbe082f95c96004acb36dd508609
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-08-20 23:13:27 +0600

    15-02swap add

---
+++ 15 JAVASCRIPT CODING PROBLEMS, SIMPLE INTERVIEW QUESTION/15-01swap.js
@@ -0,0 +1,28 @@
+// ******temp methor*******
+
+// var a = 5;
+// var b = 7;
+
+// var temp = a;
+// a = b;
+// b = temp;
+
+// console.log(a, b);
+
+// var x = 5;
+// var y = 7;
+
+// x = x + y;
+// y= x-y;
+// x = x -y;
+
+
+// console.log(x, y);
+
+// *********swap java methor**********
+
+var x = 5;
+var y = 7;
+
+[x, y]=[y, x];
+console.log(x, y);
