a2e20f873a678d5988bf15a4b52698747a36e323
Arman Hossain Akash <armanhossainakash52@gmail.com>
2023-09-04 23:37:49 +0600

    19-11

---
+++ 19FUNCTION, ADDEVENTLISTENER, EVENT BUBBLE/19-11Event.js
@@ -0,0 +1,12 @@
+
+  document.getElementById('myBTN').addEventListener('click', function(even){
+    var itmeParent = document.getElementById('myList');
+    var newItme = document.createElement('li');
+    newItme.innerText = 'Brand New';
+    itmeParent.appendChild(newItme);
+  });
+  
+  document.getElementById('myList')
+  .addEventListener('click', function(event){
+    event.target.parentNode.removeChild(event.target);
+  });
